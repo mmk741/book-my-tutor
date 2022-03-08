f69938dd645d91c2f94b6b8d6084124d2837dfee
Asif <asifanwar0000@outlook.com>
2022-03-08 12:37:58 +0530

    Project initated

---
+++ booktutor/src/main/java/com/teacherbooking/booktutor/BooktutorApplication.java
@@ -0,0 +1,13 @@
+package com.teacherbooking.booktutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooktutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooktutorApplication.class, args);
+	}
+
+}
---
+++ booktutor/src/test/java/com/teacherbooking/booktutor/BooktutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teacherbooking.booktutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooktutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
