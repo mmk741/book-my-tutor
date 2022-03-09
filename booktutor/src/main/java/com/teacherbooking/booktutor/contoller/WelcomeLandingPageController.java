7ae2879da905f56f9ff73a1414e4e3b731c1d4bd
moshahid <moshahidkhan741@gmail.com>
2022-03-10 00:27:14 +0530

    message service added

--- booktutor/src/main/java/com/teacherbooking/booktutor/contoller/WelcomeLandingPageController.java
+++ booktutor/src/main/java/com/teacherbooking/booktutor/contoller/WelcomeLandingPageController.java
@@ -1,8 +1,18 @@
 package com.teacherbooking.booktutor.contoller;
 
 import com.teacherbooking.booktutor.constant.Constant;
+import com.teacherbooking.booktutor.dto.MailRequest;
+import com.teacherbooking.booktutor.dto.MailResponse;
+import com.teacherbooking.booktutor.services.EmailService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +24,17 @@ public class WelcomeLandingPageController {
     public String welcome(){
         return "Book your tutor in a min";
     }
+    
+    
+    @Autowired
+	private EmailService service;
+
+	@PostMapping("/sendingEmail")
+	public MailResponse sendEmail(@RequestBody MailRequest request) {
+		Map<String, Object> model = new HashMap<>();
+		model.put("Name", request.getName());
+		model.put("location", "Patna Bihar");
+		return service.sendEmail(request, model);
+
+	}
 }
